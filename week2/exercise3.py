24150dd24d21e50ff2b5acb0d1beb9c4d11657a6
Nick Pan <nicolaspan98@gmail.com>
2020-06-16 17:34:24 +1000

    week 2 exercise 3

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,8 +7,11 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-   answer = 5 % 2 = 1
-    return answer
+    if a_number % 2 ==0:
+        return False
+    else:
+        return True
+       
 
 
 
@@ -27,8 +30,15 @@ def fix_it(moves=True, should_move=True):
     Most people write this function with 4 return statements. 
     As an extra challenge, see if you can get that down to three.
     """
-    return None
-
+    if moves and should_move:
+        return "No Problem"
+    elif moves and not should_move:
+        return "Duct Tape"
+    elif not moves and not should_move:
+        return "No Problem"
+    elif not moves and should_move:
+        return "WD-40"
+    
 
 def loops_1a():
     """Make 10 stars.
@@ -37,7 +47,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    list = []
+    for x in range(10): 
+        list.append("*")
+    return list 
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -47,7 +60,10 @@ def loops_1c(number_of_items=5, symbol="#"):
     string with exacly one symbol in it.
     E.g.: ['#', '#', '#', '#', '#']
     """
-    return None
+    list = []
+    for x in range(number_of_items):
+        list.append(symbol)
+    return list
 
 
 def loops_2():
@@ -68,7 +84,10 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    list= []
+    for x in range(10): 
+        list.append(loops_1c(number_of_items=10, symbol="*"))
+    return list
 
 
 def loops_3():
@@ -76,7 +95,7 @@ def loops_3():
 
     Return this:
     [
-        ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+         
         ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
         ['2', '2', '2', '2', '2', '2', '2', '2', '2', '2'],
         ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3'],
@@ -92,7 +111,13 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    list= []
+    for x in range(10):
+        row = []
+        for i in range(10):
+            row.append(str(x))
+        list.append(row)
+    return list
 
 
 def loops_4():
@@ -112,7 +137,13 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+    list= []
+    for x in range(10):
+        row = []
+        for i in range(10):
+            row.append(str(i))
+        list.append(row)
+    return list
 
 
 def loops_5():
@@ -139,7 +170,14 @@ def loops_5():
         "There are {} green bottles".format(8)
     you'll come to see the pros and cons of each over time.
     """
-    return None
+    list= []
+    for i in range(10):
+        row = []
+        for j in range(5):
+            row.append("(i{}, j{})".format(i, j))
+        list.append(row)
+    return list
+    
 
 
 def loops_6():
@@ -162,7 +200,14 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    return None
+    wedge= []
+    for i in range(10):
+        row = []
+        for j in range(i + 1): 
+            row.append(str(j))
+        wedge.append(row)
+    print(wedge)
+    return wedge
 
 
 def loops_7():
@@ -186,8 +231,13 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    return None
+    pyramid = []
+    for i in range(5):
+        row = '{0}{1}{0}'.format(' '*(5-i-1), '*'*(i*2+1))
+        pyramid.append(list(row))
 
+    print (pyramid)
+    return pyramid
 
 def lp(some_kind_of_list, exercise_name):
     """Help to see what's going on.
--- week2/readme.md
+++ week2/readme.md
@@ -1,1 +1,8 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+Argument= puts value in
+return = gives value back
+()= used for calling functions and order of operations
+[]= used for lists
+{}= used for to define dictionary. Something that maps a value to another
+casting = convert variable from one type to another type
+append= add something to the end
